1ff450dc181e90a95c5283ed77904e56544fb49c
ratchanon2318 <ratchanon.noknoy2318@gmail.com>
2026-01-15 16:59:27 +0700

    path

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   // Configure `basePath` and `assetPrefix` for GitHub Pages.
   // The repo name is 'resume'.
-  basePath: '/resume',
-  assetPrefix: '/resume/',
+  basePath: isProd ? '/resume' : undefined,
+  assetPrefix: isProd ? '/resume/' : undefined,
   // Since we are using `next export`, we must disable image optimization.
   images: {
     unoptimized: true,
